0cca2ecae814942d2db8c945ab67c75afe3da12e
ainunazizah350 <ainunazizah15990@gmail.com>
2021-04-19 20:25:54 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -62,7 +62,8 @@
                 number with anyone else.</small>
             </div>
 
-            <button type="submit" name="submit" class="btn btn-success btn-block">Save Changes</button>
+            <a href="viewrecords.php" class="btn btn-default">Back to List</a>
+            <button type="submit" name="submit" class="btn btn-success">Save Changes</button>
         </form>
         
     <?php } ?>
